4134289e535b52920ef09a445fd735dc331f2232
yoda-mon <yodal@oss.nttdata.com>
2022-08-03 08:51:11 +0000

    add docs to test github pages

---
+++ docs/assets/js/031793e1.d282b5c1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbigtop_site=self.webpackChunkbigtop_site||[]).push([[1633],{2511:e=>{e.exports=JSON.parse('{"label":"facebook","permalink":"/blog/tags/facebook","allTagsPath":"/blog/tags","count":1}')}}]);
